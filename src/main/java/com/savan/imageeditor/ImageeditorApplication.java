fdc9502b5f5d8c4802e8784662043c6e01b872f2
bartoszsawaniewski <savan2073@gmail.com>
2024-11-22 09:28:22 +0100

    init

---
+++ src/main/java/com/savan/imageeditor/ImageeditorApplication.java
@@ -0,0 +1,13 @@
+package com.savan.imageeditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageeditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageeditorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/savan/imageeditor/ImageeditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savan.imageeditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageeditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
